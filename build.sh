40c3d224433ac54eadeec0c03869abe5486e5e28
taikulawo <qaq1362211689@gmail.com>
2023-12-28 20:18:55 +0800

    1

--- build.sh
+++ build.sh
@@ -3,13 +3,12 @@ set -xe
 export DPDK_NAME=dpdk
 export PKG_CONFIG_PATH=$(pwd)/$DPDK_NAME/dpdklib/lib/x86_64-linux-gnu/pkgconfig
 
-HOME=$(pwd)
+WORKDIR=$(pwd)
 
 cd $DPDK_NAME
-# 高版本bear用 bear -- bash -c
-bear ninja -C dpdkbuild\
-&& cd dpdkbuild && ninja install\
-&& cd $HOME \
-&& make -j$(nproc)
-cd $HOME;
+ninja  -C dpdkbuild -t compdb > $WORKDIR/compile_commands.json
+cd dpdkbuild && ninja install
+cd $WORKDIR
+# 高版本bear用 bear -a --
+bear -a make -j$(nproc)
 make install
