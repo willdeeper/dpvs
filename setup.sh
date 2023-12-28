128167bfd76e6427bd86a39198e7e76128be1e84
taikulawo <qaq1362211689@gmail.com>
2023-12-28 18:55:05 +0800

    1

--- setup.sh
+++ setup.sh
@@ -7,7 +7,11 @@ sudo apt remove libdpdk-dev -y
 sudo apt autoremove
 sudo apt install pkg-config python3-pip meson ninja-build libnuma-dev libpopt-dev linux-headers-$(uname -r) -y
 pip3 install pyelftools
-cp patch/dpdk-stable-20.11.1/*.patch dpdk
 cd dpdk
+git clean -df
+git checkout .
+cp ../patch/dpdk-stable-20.11.1/*.patch ./
 patch -p1 < 0001-kni-use-netlink-event-for-multicast-driver-part.patch
 patch -p1 < 0002-pdump-change-dpdk-pdump-tool-for-dpvs.patch
+rm -rf dpdk/dpdklib
+rm -rf dpdk/dpdkbuild
