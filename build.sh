0db711e0989c17c9332ffe99ff981462f811c836
taikulawo <qaq1362211689@gmail.com>
2023-12-28 17:50:28 +0800

    1

--- build.sh
+++ build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+set -xe
 export DPDK_NAME=dpdk
-export PKG_CONFIG_PATH=$(pwd)/$DPDK_NAME/dpdklib/lib/x86_64-linux-gnu/pkgconfig
+
 HOME=$(pwd)
 
 cd $DPDK_NAME
-mkdir dpdklib                 # user desired install folder
-mkdir dpdkbuild               # user desired build folder
+[ -d "dpdklib" ] || mkdir -p dpdklib
+[ -d "dpdkbuild" ] || mkdir dpdkbuild
 meson -Denable_kmods=true -Dprefix=$(pwd)/dpdklib dpdkbuild
 ninja -C dpdkbuild
 cd dpdkbuild; ninja install
-
+export PKG_CONFIG_PATH=$(pwd)/$DPDK_NAME/dpdklib/lib/x86_64-linux-gnu/pkgconfig/libdpdk.pc
 cd $HOME
 make -j$(nproc)
 make install
--- setup.sh
+++ setup.sh
@@ -2,11 +2,12 @@
 git submodule update --init --remote --recursive --progress
 # 删除debian内置的dpdk
 # 避免include header冲突
+sudo apt update
 sudo apt remove libdpdk-dev -y
 sudo apt autoremove
-sudo apt install meson ninja-build libnuma-dev libpopt-dev linux-headers-$(uname -r) -y
+sudo apt install pkg-config meson ninja-build libnuma-dev libpopt-dev linux-headers-$(uname -r) -y
 
 cp patch/dpdk-stable-20.11.1/*.patch dpdk
 cd dpdk
-patch -p1 -f < 0001-kni-use-netlink-event-for-multicast-driver-part.patch
-patch -p1 -f < 0002-pdump-change-dpdk-pdump-tool-for-dpvs.patch
+patch -p1 < 0001-kni-use-netlink-event-for-multicast-driver-part.patch
+patch -p1 < 0002-pdump-change-dpdk-pdump-tool-for-dpvs.patch
