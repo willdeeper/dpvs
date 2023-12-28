ee804b815f0893c2ea2ef3e2c27fac870276637f
taikulawo <qaq1362211689@gmail.com>
2023-12-28 13:02:06 +0800

    1

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 HOME=$(pwd)
-apt install meson ninja-build linux-headers-$(uname -r) -y
-cp patch/dpdk-stable-20.11.1/*.patch dpdk-stable-20.11.1/
-cd dpdk-stable-20.11.1/
-patch -p1 < 0001-kni-use-netlink-event-for-multicast-driver-part.patch
-patch -p1 < 0002-pdump-change-dpdk-pdump-tool-for-dpvs.patch
-cd ../
 cd dpdk-stable-20.11.1
 mkdir dpdklib                 # user desired install folder
 mkdir dpdkbuild               # user desired build folder
---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt install meson ninja-build linux-headers-$(uname -r) -y
+cp patch/dpdk-stable-20.11.1/*.patch dpdk-stable-20.11.1/
+cd dpdk-stable-20.11.1/
+patch -p1 -f < 0001-kni-use-netlink-event-for-multicast-driver-part.patch
+patch -p1 -f < 0002-pdump-change-dpdk-pdump-tool-for-dpvs.patch
