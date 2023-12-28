e834ac4d876f194cca8fed8d349de158f704cf36
taikulawo <qaq1362211689@gmail.com>
2023-12-28 15:49:04 +0800

    1

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+DPDK_NAME=dpdk-stable-20.11.1
 HOME=$(pwd)
-cd dpdk
+cd $DPDK_NAME
 mkdir dpdklib                 # user desired install folder
 mkdir dpdkbuild               # user desired build folder
 meson -Denable_kmods=true -Dprefix=$(pwd)/dpdklib dpdkbuild
 ninja -C dpdkbuild
 cd dpdkbuild; ninja install
-export PKG_CONFIG_PATH=$(pwd)/dpdk/dpdklib/lib/x86_64-linux-gnu/pkgconfig
+export PKG_CONFIG_PATH=$(pwd)/$DPDK_NAME/dpdklib/lib/x86_64-linux-gnu/pkgconfig
 cd $HOME
 make -j$(nproc)
